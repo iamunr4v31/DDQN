5e8ba5c2d0903c8947d293e91b4de63d7306c184
iamunr4v31 <ashwins1211@gmail.com>
2022-08-02 12:54:31 +0530

    DDQN forked from DQN

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import sys
 import numpy as np
-from DQNAgent import Agent
+from DDQNAgent import Agent
 from utils import plot_learning_curve
 from preprocess import make_env
 import wandb
-import torch.nn as nn
+
 
 if __name__ == "__main__":
-    env = make_env("PongNoFrameskip-v4")
-    wandb.init(project="pong-dqn")
+    env_name = "BankHeistNoFrameskip-v4"
+    env = make_env(env_name)
+    wandb.init(project="pong-ddqn")
     best_score = -np.inf
     load_checkpoint = False
     n_games = 500
-    agent = Agent(gamma=0.99, epsilon=0.1, lr= 0.0001, 
+    agent = Agent(gamma=0.99, epsilon=1.0, lr= 0.0001, 
                 input_dims=env.observation_space.shape, n_actions=env.action_space.n,
                 memory_size=30000, eps_min=0.1, decay_rate=1e-5, batch_size=32,
-                replace=1000, checkpoint_dir='models/', algo="DQNAgent", env_name="PongNoFrameskip-v4")
+                replace=1000, checkpoint_dir='models/', algo="DDQNAgent", env_name=env_name)
     wandb.config = agent.__dict__
-    agent.load_models()             # comment after training
+    # agent.load_models()             # comment after training
     
     if load_checkpoint:
         agent.load_models()
@@ -50,9 +51,10 @@ if __name__ == "__main__":
 
         avg_score = np.mean(scores[-100:])
         if np.mean(scores[-100:]) >= np.mean(scores[-101:-1]):
-            best_score = score
             if not load_checkpoint:
                 agent.save_models()
+        if score > best_score:
+            best_score = score
         
 
         sys.stdout.write(f"Game: {i}/{n_games} | Score: {score:.2f} | Average Score: {avg_score:.2f} | Best Score: {best_score} | Epsilon: {agent.epsilon:.2f} | steps: {n_steps}\n")
